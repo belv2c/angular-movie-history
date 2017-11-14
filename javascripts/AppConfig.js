ab0d0af991564abf38ba95ce329d00f9c39df053
Brooke Lewis <belv2c@gmail.com>
2017-11-13 20:52:36 -0600

    added navbar and links

---
+++ javascripts/AppConfig.js
@@ -0,0 +1,27 @@
+"use strict";
+
+app.config(function($routeProvider){
+	$routeProvider
+		.when("/auth", {
+			// path to html file and path to javascript file
+			templateUrl: 'partials/auth.html',
+			controller: 'AuthCtrl'
+		})
+		.when("/search", {
+			// path to html file and path to javascript file
+			templateUrl: 'partials/search.html',
+			controller: 'SearchCtrl'
+		})
+		.when("/wishlist", {
+			// path to html file and path to javascript file
+			templateUrl: 'partials/wishlist.html',
+			controller: 'WishlistCtrl'
+		})
+		.when("/rated", {
+			// path to html file and path to javascript file
+			templateUrl: 'partials/rated.html',
+			controller: 'RatedCtrl'
+		})
+		// if your user tries to type in any other route besides what you've defined you can redirect them
+		.otherwise('/auth');
+});
