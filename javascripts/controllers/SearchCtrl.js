12f544f65143ae387b45140f71f96bfa26a7d713
Brooke Lewis <belv2c@gmail.com>
2017-11-15 20:56:23 -0600

    movie adds to rated page

--- javascripts/controllers/SearchCtrl.js
+++ javascripts/controllers/SearchCtrl.js
@@ -1,7 +1,20 @@
 "use strict";
 
-app.controller("SearchCtrl", function($scope, tmdbService){
+app.controller("SearchCtrl", function($location, $rootScope, $scope, MovieService, tmdbService){
 	$scope.movies = [];
+
+	const createMovie = (movie) => {
+		return {
+			"title": movie.title,
+			"overview": movie.overview,
+			"poster_path": movie.poster_path,
+			"rating": 0,
+			"isWatched": true,
+			"uid": $rootScope.uid
+		};
+
+	};
+
 
 	$scope.enterPush = (event) => {
 		if(event.keyCode === 13){
@@ -13,7 +26,21 @@ app.controller("SearchCtrl", function($scope, tmdbService){
 	});
   }
 };
+
+
+
+$scope.saveRated = (tmdbMovie) => {
+	console.log("tmdbMovie", tmdbMovie);
+	let newMovie = createMovie(tmdbMovie);
+	MovieService.postNewMovie(newMovie).then(() => {
+		$location.path('/rated');
+	}).catch((err) => {
+		console.log("error in postNewMovie", err);
+	});
+};
+
+
+
+
+
 });
-
-
-	
